89a55071a8fa70c5d4dd211499fe3486f9106fde
Drake Costa <drake@saeris.io>
2020-03-23 02:45:31 -0700

    Update Apollo Service Name

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   service: {
     endpoint: {
-      name: `launchpad-lk3jvvvjnq`,
+      name: `scryfall`,
       url: `http://localhost:1337/${
         process.env.NETLIFY ? `.netlify/functions/scryfall-api/` : `dev`
       }`
